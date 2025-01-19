2d795b33f208de99981a0553530303acc232ebb1
suarez0999 <diegos6154830@gmail.com>
2025-01-18 19:08:26 -0500

    Update color scheme to use custom blue shades; adjust button and section backgrounds for consistency

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class', // o 'media' si prefieres usar la preferencia del sistema
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // o 'media' si prefieres usar la preferencia del sistema
+  theme: {
+    extend: {
+     
+    },
+  },
+  plugins: [],
+};
