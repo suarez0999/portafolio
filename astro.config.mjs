b7a2f39cd08d936a04b020313ac53c56a084a03d
suarez0999 <diegos6154830@gmail.com>
2025-01-30 12:33:54 -0500

    Add path aliases in Astro configuration; remove unused h.html file and clean up tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": "/src/components",
+        "@ui": "/src/components/ui",
+        "@icons": "/src/components/icons",
+        "@images": "/public/images",
+        "@layouts": "/src/layouts",
+        "@pages": "/src/pages",
+        "@fonts": "/src/fonts",
+      },
+    },
+  },
 });
